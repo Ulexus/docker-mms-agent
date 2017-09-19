8dcc5d0a2662fe2bfa436787ed76f02b84e04399
Seán C McCord <ulexus@gmail.com>
2017-09-18 20:49:04 -0400

    add new GroupId

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 mms-agent
 ==========
 
-MongoDB Monitoring Service (mms) agent
+MongoDB Monitoring Service (mms), now MongoDB Cloud Manager, agent
 
 Environment variables
 ---------------------
 
-*  `MMS_API_KEY`  _(required)_ Your mms API key
+*  `MMS_API_KEY`  _(required)_ Your MongoDB Cloud Manager API key
+*  `MMS_GROUP_ID` _(required)_ Your MongoDB Cloud Manager Group ID
 *  `MMS_SERVER`  Optional override of the mms server URL to connect to
 *  `MMS_MUNIN`   (boolean, default: `true`)  Whether to use munin
 *  `MMS_CHECK_SSL_CERTS` (boolean, default: `true`) Whether to check SSL certificates
--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,15 @@ if [ ! "$MMS_API_KEY" ]; then
 	exit 1
 fi
 
+if [ ! "$MMS_GROUP_ID" ]; then
+	{
+		echo 'error: MMS_GROUP_ID was not specified'
+		echo 'try something like: docker run -e MMS_GROUP_ID=... ...'
+		echo '(see Mongo Cloud Console for your mmsGroupId)'
+	} >&2
+	exit 1
+fi
+
 # "sed -i" can't operate on the file directly, and it tries to make a copy in the same directory, which our user can't do
 config_tmp="$(mktemp)"
 cat /etc/mongodb-mms/monitoring-agent.config > "$config_tmp"
@@ -31,6 +40,7 @@ set_config() {
 }
 
 set_config mmsApiKey "$MMS_API_KEY"
+set_config mmsGroupId "$MMS_GROUP_ID"
 set_config mmsBaseUrl "$MMS_SERVER"
 set_config enableMunin "$MMS_MUNIN"
 set_config sslRequireValidServerCertificates "$MMS_CHECK_SSL_CERTS"
